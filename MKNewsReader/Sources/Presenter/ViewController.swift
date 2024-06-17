8ab7dc11a436ec1e904f9ba903e02cede87b9e24
Steve M. Kim <kmkhaha@nate.com>
2024-06-18 00:48:19 +0900

    Initialize project

---
+++ MKNewsReader/Sources/Presenter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MKNewsReader
+//
+//  Created by MK on 6/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ MKNewsReader/Sources/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  MKNewsReader
+//
+//  Created by MK on 6/17/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        let vc = ViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
